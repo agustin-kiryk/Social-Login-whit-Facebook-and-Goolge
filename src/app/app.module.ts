62d3b9cd646879e4a0cb3f57454c1a098181cfff
agustin-kiryk <agustin.kiryk@gmail.com>
2022-10-13 19:34:48 -0300

    credenciales google y facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//Social Login
+//Social Login google y facebook
 import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
 import {
   GoogleLoginProvider,
