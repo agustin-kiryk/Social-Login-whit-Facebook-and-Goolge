d58f9b0cfdc8ea85bb7bc06b7b4b804b5f8348d7
agustin-kiryk <agustin.kiryk@gmail.com>
2022-10-14 23:30:47 -0300

    ver metodos deprecados

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialAuthService, GoogleLoginProvider, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  socialUser : SocialUser | undefined;
+  userLogged : SocialUser | undefined;
+  isLogged : Boolean | undefined;
+
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe(
+      data => {
+        this.userLogged = data;
+        this.isLogged = (this.userLogged != null);
+      }
+    )
   }
 
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      data => {
+       this.socialUser = data;
+       this.isLogged = true
+      }
+    );
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(
+      data => {
+        this.socialUser = data;
+        this.isLogged = true
+      }
+    );
+  }
+  
+  logOut(): void{
+    this.authService.signOut();
+  }
 }
